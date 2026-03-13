56e204858c5169ba43862ee3dcdd8f1d031d139f
KingAmada <sarkiamada@gmail.com>
2026-03-13 16:43:29 +0100

    Add files via upload

--- app.config.js
+++ app.config.js
@@ -1,6 +1,9 @@
 window.POOLILY_RUNTIME_CONFIG = {
   mapboxToken: "pk.eyJ1IjoiYWVyb3Bvb2wiLCJhIjoiY21tamRpY2N6MWkycDJvczZ5dmVkYjZiaiJ9.Vx88t1T4vmyJhcHWT9Bggg",
-  mapStyle: "mapbox://styles/mapbox/navigation-day-v1",
+  mapStyle: "mapbox://styles/mapbox/standard",
   mapDefaultZoom: 14.5,
-  mapRouteMaxZoom: 15.5,
+  mapRouteMaxZoom: 16.5,
+  faceMatchMaxDistance: 0.42,
+  faceMinDetectionScore: 0.8,
+  trackingArrivalRadiusMeters: 50,
 };
